1adca796a291238ff006dc75b930f440e869f357
Maxe1 <49842974+Maxe1@users.noreply.github.com>
2019-04-21 23:45:13 +0300

    Done

---
+++ Films_Library/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace Films_Library
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openXmlToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.resetToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 33);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(378, 102);
+            this.textBox1.TabIndex = 0;
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem,
+            this.resetToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(905, 24);
+            this.menuStrip1.TabIndex = 1;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openXmlToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // openXmlToolStripMenuItem
+            // 
+            this.openXmlToolStripMenuItem.Name = "openXmlToolStripMenuItem";
+            this.openXmlToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.openXmlToolStripMenuItem.Text = "Open xml...";
+            this.openXmlToolStripMenuItem.Click += new System.EventHandler(this.openXmlToolStripMenuItem_Click);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(134, 22);
+            this.exitToolStripMenuItem.Text = "Exit..";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.aboutToolStripMenuItem.Text = "About...";
+            this.aboutToolStripMenuItem.Click += new System.EventHandler(this.aboutToolStripMenuItem_Click);
+            // 
+            // resetToolStripMenuItem
+            // 
+            this.resetToolStripMenuItem.Name = "resetToolStripMenuItem";
+            this.resetToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.resetToolStripMenuItem.Text = "reset";
+            this.resetToolStripMenuItem.Click += new System.EventHandler(this.resetToolStripMenuItem_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(396, 33);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(497, 310);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 144);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(378, 199);
+            this.listBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(905, 347);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Xml Viewer";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem openXmlToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem resetToolStripMenuItem;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ Films_Library/Form1.cs
@@ -0,0 +1,366 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Security;
+using System.Linq;
+using System.Windows.Forms;
+using System.Xml;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using InterfacePlugin;
+using System.Xml.Linq;
+using System.Text.RegularExpressions;
+
+namespace Films_Library
+{
+    public partial class Form1 : Form
+    {
+        private string path = null;
+        private OpenFileDialog opendialog;
+        private readonly string pluginPath = System.IO.Path.Combine(Directory.GetCurrentDirectory(), "Plugins");
+        private List<IPlugin> plugins = new List<IPlugin>();
+        private List<OutCheckBox> filters = new List<OutCheckBox>();
+
+        public Form1()
+        {
+            InitializeComponent();
+            RefreshPlugins();
+        }
+
+        private void RefreshPlugins()
+        {
+            plugins.Clear();
+            DirectoryInfo pluginDirectory = new DirectoryInfo(pluginPath);
+            if (!pluginDirectory.Exists)
+                pluginDirectory.Create();
+            //берем из директории все файлы с расширением .dll      
+            var pluginFiles = Directory.GetFiles(pluginPath, "*.dll");
+            foreach (var file in pluginFiles)
+            {
+                //загружаем сборку
+                Assembly asm = Assembly.LoadFrom(file);
+                //ищем типы, имплементирующие наш интерфейс IPlugin,
+                //чтобы не захватить лишнего
+                var types = asm.GetTypes().
+                                Where(t => t.GetInterfaces().
+                                Where(i => i.FullName == typeof(IPlugin).FullName).Any());
+
+                //заполняем экземплярами полученных типов коллекцию плагинов
+                foreach (var type in types)
+                {
+                    var plugin = asm.CreateInstance(type.FullName) as IPlugin;
+                    plugins.Add(plugin);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Output information from file to fields with plugins
+        /// working by query and method SetTextList
+        /// </summary>
+        /// <param name="flag">set true or false if you have plugins or not</param>
+        public void emptyPlugin(bool flag)
+        {
+            Form1 form = new Form1();
+            form.listBox1.Items.Add(" ");
+            XDocument xdoc = XDocument.Load(path);
+            MovieList actor = new MovieList();
+
+            var items = from xe in xdoc.Element("movies").Elements("movie")
+                        select new
+                        {
+                            Title = xe.Element("title").Value,
+                            Year = xe.Element("year").Value,
+                            Country = xe.Element("country").Value,
+                            Genre = xe.Element("genre").Value,
+                            Director = String.Join(" ", xe.Element("director").Element("first_name").Value, xe.Element("director").Element("last_name").Value, xe.Element("director").Element("birth_date").Value),
+                            Actor = String.Join(" ", xe.Element("actor").Element("first_name").Value, xe.Element("actor").Element("last_name").Value, xe.Element("actor").Element("birth_date").Value),
+
+                            actor = from xe1 in xe.Elements("actor")
+                                    select new MovieList
+                                    {
+                            First_name = xe1.Element("first_name").Value,
+                            Last_name = xe1.Element("last_name").Value,
+                            Birth_date = xe1.Element("birth_date").Value}
+                        };
+
+
+            if (flag == true)
+            {
+                foreach (var item in items)
+                {
+                    SetTextList(item.Title, item.Year, item.Country, item.Genre, item.Director, item.Actor);
+                }
+            }
+            else
+            {
+                foreach (var item in items)
+                {
+                    listBox1.Items.Add(item.Title);
+                }  
+            }
+        }
+        /// <summary>
+        /// Work with plugins and open file *.xml in OpnDialog method.
+        /// </summary>
+        public void openXmlToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+            path = OpnDialog();
+            if (path == null)
+            {
+                MessageBox.Show("file doesn't exist ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                opendialog.Reset();
+            }
+            else
+            {
+                List<string> nameFilm = new List<string>();
+                bool flag = true;
+                listBox1.Items.Clear();
+                flowLayoutPanel1.Controls.Clear();
+                if (plugins.Count == 0)
+                {
+
+                    try
+                    {
+                        emptyPlugin(flag);
+                    }
+                    catch (SecurityException ex)
+                    {
+                        MessageBox.Show("Security error.\n\nError message: {ex.Message}\n\n" +
+                        "Details:\n\n{ex.StackTrace}" + ex);
+                    }
+                }
+                else
+                {
+                    flag = false;
+                    emptyPlugin(flag);
+                    outplugins(path);
+                }
+            }
+        }
+
+        public class MovieList
+        {
+            public string First_name { get; set; }
+            public string Last_name { get; set; }
+            public string Birth_date { get; set; }
+        }
+
+        /// <summary>
+        /// Method for output information on the list and text box.
+        /// </summary>
+        public void SetTextList(string title, string year,string country,string genre,string director,string actor) {
+            textBox1.Text = textBox1.Text + ("Film: " + title + Environment.NewLine + "Year: " + year + Environment.NewLine
+            + "Country: " + country + Environment.NewLine + "Genre: " + genre + Environment.NewLine + "Director: " + director
+            + Environment.NewLine + "Actor: " + actor + Environment.NewLine);
+            textBox1.Text = textBox1.Text + Environment.NewLine;
+            listBox1.Items.Add(title);
+        }
+    
+        public string RemoveSpace(string str) {
+            var res = str.Replace(" ", "");
+            return res;
+        }
+        /// <summary>
+        /// One of main method of program are filtering information from movie file and show on box to depending on the filter.
+        /// </summary>
+        public void show()
+        {
+           XDocument xdoc = XDocument.Load(path);
+           var value = xdoc.Element("movies").Elements("movie");
+           var qwerty = from xe in xdoc.Element("movies").Elements("movie")
+                       select new
+                       {
+                           Title = xe.Element("title").Value,
+                           Year = xe.Element("year").Value,
+                           Country = xe.Element("country").Value,
+                           Genre = xe.Element("genre").Value,
+                           Director = String.Join(" ", xe.Element("director").Element("first_name").Value, xe.Element("director").Element("last_name").Value, xe.Element("director").Element("birth_date").Value),
+                           actor = from xe1 in xe.Elements("actor")
+                                   select new MovieList
+                                   {
+                                       First_name = xe1.Element("first_name").Value,
+                                       Last_name = xe1.Element("last_name").Value,
+                                       Birth_date = xe1.Element("birth_date").Value
+                                   }
+                       };
+            
+
+           foreach (var index in this.filters) 
+           {
+               if (index.select.Count > 0)       
+               {
+                   if (index.name == "actor") {
+                       List<string> films = new List<string>();
+                       foreach (var aa in qwerty)
+                       {
+                           if (aa.actor.Any(k => index.select.Exists(y => y.Contains(k.First_name))))
+                           {
+                               films.Add(aa.Title);
+                           }
+                       }              
+                       value.Where(x => !films.Exists(y => x.Value.Contains(y))).Remove();
+                   }
+                   else
+                   {
+                       value.Where(x => !(RemoveSpace(String.Join("", index.select))).Contains(x.Element(index.name).Value)).Remove();
+                   }
+               }
+           }
+           var items = from xe in value
+                       select new Movie
+                       {
+                           Title = xe.Element("title").Value,
+                           Year = xe.Element("year").Value,
+                           Country = xe.Element("country").Value,
+                           Genre = xe.Element("genre").Value,
+                           Director = String.Join(" ", xe.Element("director").Element("first_name").Value, xe.Element("director").Element("last_name").Value, xe.Element("director").Element("birth_date").Value),
+                           Actor = String.Join(" ", xe.Element("actor").Element("first_name").Value, xe.Element("actor").Element("last_name").Value, xe.Element("actor").Element("birth_date").Value),
+                       };
+
+            listBox1.Items.Clear();
+            textBox1.Text = "";
+                foreach (var item in items)
+                {
+                    SetTextList(item.Title, item.Year, item.Country , item.Genre,  item.Director,item.Actor );
+                }
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+
+        private void resetToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+            listBox1.Items.Clear();
+
+        }
+
+        private void aboutToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Films List App", "About...", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+        /// <summary>
+        /// The method draw our checkdox list on the form, depending onthe how many plugins consist of directory Plugins
+        /// </summary>
+        /// <param name="path">The file path for openDialog</param>
+        private void outplugins(string path)
+        {
+            try
+            {
+                foreach (var plugin in plugins)
+                {
+                    var control = new OutCheckBox(plugin, path, this);
+                    this.filters.Add(control);
+                    flowLayoutPanel1.Controls.Add(control);
+                }
+            }
+
+            catch (SecurityException ex)
+            {
+                MessageBox.Show("Security error.\n\nError message: {ex.Message}\n\n" +
+                "Details:\n\n{ex.StackTrace}" + ex);
+            }
+
+        }
+        
+
+        private string OpnDialog()
+        {
+            opendialog = new OpenFileDialog();
+            opendialog.Filter = "XML Files (*.xml)|*.xml";
+            opendialog.FilterIndex = 1;
+            opendialog.Multiselect = false;
+            if (opendialog.ShowDialog() == DialogResult.OK)
+            {
+                path = opendialog.FileName;
+            }
+            else
+            {
+                DialogResult dialogResult = MessageBox.Show("Open file *.xml", "Error", MessageBoxButtons.OKCancel, MessageBoxIcon.Error);
+                if (dialogResult == DialogResult.OK)
+                {
+                    OpnDialog();
+                }
+                if (dialogResult == DialogResult.Cancel)
+                {
+                    opendialog.Reset();
+
+                }
+            }
+
+            return path;
+        }
+
+    }
+
+
+    public class OutCheckBox : UserControl
+    {
+        public List<string> select = new List<string>();
+        public string name;
+        private Form1 form;
+
+        public OutCheckBox(IPlugin plugin, string path, Form1 form)
+        {
+            this.form = form;
+            InitializeComponent();
+            object[] array = plugin.OutPut(path).Cast<Object>().ToArray();
+            this.name = plugin.name();
+            this.label1.Text = this.name.ToUpper() + ":";
+            checkedListBox1.Items.AddRange(array);
+            checkedListBox1.CheckOnClick = true;
+        }
+
+        void checkedListBox1_ItemCheck(object sender, ItemCheckEventArgs e) //  
+        {
+            if (e.NewValue == CheckState.Checked)
+                this.select.Add(checkedListBox1.Items[e.Index].ToString());
+            else
+                this.select.Remove(checkedListBox1.Items[e.Index].ToString());
+
+            this.form.show();
+        }
+
+        private void InitializeComponent()
+        {
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout(); 
+            this.checkedListBox1.Location = new System.Drawing.Point(10, 20);
+            this.checkedListBox1.Size = new System.Drawing.Size(140, 125);
+            this.checkedListBox1.ItemCheck += new ItemCheckEventHandler(this.checkedListBox1_ItemCheck);
+            this.checkedListBox1.TabIndex = 0;
+            this.label1.Location = new System.Drawing.Point(10, 5);
+            this.Controls.Add(this.checkedListBox1);
+            this.Controls.Add(this.label1);
+            this.Size = new System.Drawing.Size(150, 145);
+            this.ResumeLayout(false);
+        }
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.Label label1;
+
+    }
+
+    public class Filter
+    {
+        public string name;
+        public string[] values;
+    }
+
+    public class Movie
+    {
+        public string Title { get; set; }
+        public string Year { get; set; }
+        public string Country { get; set; }
+        public string Genre { get; set; }
+        public string Director { get; set; }
+        public string Actor { get; set; }
+    }
+
+    
+}
